f471da8400bcf1a06089f486af4e418fad9896bb
HU SHUKANG <hushukang@gmail.com>
2022-10-11 19:31:56 +0900

    Initial commit

---
+++ TwitterClone/TwitterCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterCloneApp.swift
+//  TwitterClone
+//
+//  Created by 胡书康 on 2022/10/11.
+//
+
+import SwiftUI
+
+@main
+struct TwitterCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
